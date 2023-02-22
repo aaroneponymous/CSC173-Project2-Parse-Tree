a32c3aae80f3a070857b2ea35add0902035b60d2
Aaron Paul <aaron.paul.9810.9810@gmail.com>
2023-02-22 16:14:45 -0500

    Initial Commit: :)

---
+++ cmake-build-debug/CMakeFiles/Project_2_Parse_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_2_Parse_Tree.dir/bits.c.o"
+  "Project_2_Parse_Tree"
+  "Project_2_Parse_Tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_2_Parse_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
