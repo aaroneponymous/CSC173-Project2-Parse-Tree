d8347e2c2e7107237789232247c6c0e7fad88b2c
Dari Dennis <ddennis@u.rochester.edu>
2023-02-22 22:23:06 -0500

    blah blah blah

--- tdp.c
+++ tdp.c
@@ -76,6 +76,53 @@ int syncatRef(char *c){
     } else return -1;
 }
 
+//RETURNS LABEL OF SYNCAT USING INDEX FROM TABLE [i][j] =
+//actually this is useless, but keep for concept
+int int_to_syncat(int n){
+    switch(n){
+        case 3:
+        case 6:
+        case 9:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+            return 1;
+        case 1:
+        case 2:
+        case 4:
+        case 5:
+        case 7:
+        case 10:
+            return 2;
+        case 8:
+            return 3;
+        default:
+            return -1;
+    }
+}
+
+//RETURNS LABEL OF SYNCAT USING INDEX FROM TABLE [i][j]
+char* index_to_syncat(int n){
+    if(n == 0){
+        return "E";
+    } else if(n == 1){
+        return "ET";
+    } else if(n == 2){
+        return "T";
+    } else if(n == 3){
+        return "TT";
+    } else if(n == 4){
+        return "F";
+    } else if(n == 5){
+        return "S";
+    } else if(n == 6){
+        return "ST";
+    } else if(n == 7){
+        return "B";
+    } else return NULL;
+}
+
 //should the stack pop a terminal, use these cases to refer to a column in the table
 int charRef(char c){
     switch(c){
@@ -94,49 +141,86 @@ int charRef(char c){
         case '&':
             return 6;
         default:
-            return 7;
+            return -1;
     }
 }
 
+
 TREE parseFun(char* in) {
-    // Initialize the Stack and push the start symbol
+    int index = 0;
+    int row, col, prodxnIndex;
+
     STACK stack = createStack();
     push(stack, "E");
     TREE tree = makeNode0("E");
 
     while(!isEmpty(stack)) {
-        // Get the next input
-        nextIn = LOOKAHEAD(in, 0);
+        //get next input char
+        nextIn = LOOKAHEAD(in, index);
 
-        // Get the top of the stack
         char* top = pop(stack);
 
-        // If the top of the stack is a terminal
+        //check if top is a terminal and if it matches the current input char
         if(isTerminal(top[0])) {
-            // If the next input is the same as the top of the stack
             if(isMatch(top[0])) {
                 // Remove the next input
+                makeNode0((char*) nextIn);
                 in++;
+
             } else {
                 // Error
                 printf("Parse Failed");
                 return FAILED;
             }
-        } else {
-            // Get the row and column of the parse table
-            int row = syncatRef(top);
-            int col = charRef(nextIn);
-
-            // Get the production number from the parse table
-            int prodNum = parseTable[row][col];
-
-            // If the production number is -1, error
-            if(prodNum == -1) {
-                printf("Parse Failed");
-                return NULL;
+        } else { //else if the pop is a SG, find it and return the production
+            //we want to expand the syntactic category by finding the appropriate one based on the current symbol
+            col = charRef(nextIn);
+            row  = syncatRef(top);
+            prodxnIndex = parseTable[row][col];
+            int mn = int_to_syncat(prodxnIndex);
+            if(mn == 1) {
+                makeNode1(top, )
             }
         }
     }
+
+//    // Initialize the Stack and push the start symbol
+//    STACK stack = createStack();
+//    push(stack, "E");
+//    TREE tree = makeNode0("E");
+//
+//    while(!isEmpty(stack)) {
+//        // Get the next input
+//        nextIn = LOOKAHEAD(in, 0);
+//
+//        // Get the top of the stack
+//        char* top = pop(stack);
+//
+//        // If the top of the stack is a terminal
+//        if(isTerminal(top[0])) {
+//            // If the next input is the same as the top of the stack
+//            if(isMatch(top[0])) {
+//                // Remove the next input
+//                in++;
+//            } else {
+//                // Error
+//                printf("Parse Failed");
+//                return FAILED;
+//            }
+//        } else {
+//            // Get the row and column of the parse table
+//            int row = syncatRef(top);
+//            int col = charRef(nextIn);
+//
+//            // Get the production number from the parse table
+//            int prodNum = parseTable[row][col];
+//
+//            // If the production number is -1, error
+//            if(prodNum == -1) {
+//                printf("Parse Failed");
+//                return NULL;
+//            }
+//        }
 }
 
 
